0229e91a3d4fc1eb02e8e064d74e2f821a529263
Huggable Hacker <huggablehacker@users.noreply.github.com>
2019-06-09 14:07:51 -0400

    Update domain-grep.sh

--- domain-grep.sh
+++ domain-grep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 grep -o '[A-Za-z0-9_\.-]*\.*cisco.com' index.html |sort -u > domains.txt
 
-#predicated on you dowoloading a page with a lot of links that you need a quick and consise list.
+#predicated on you downloading a page (usually with wget) with a lot of links that you need a quick and consise list. Using regualr expresions.
